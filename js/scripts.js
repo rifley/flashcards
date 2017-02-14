ea9815cc7dc361d7ea57dba1a160eae70b4eb392
rm and rw <rw@>
2017-02-14 15:26:30 -0800

    add 4 cards with js functionality

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("#javaScript").click(function() {
+    $("#term1").toggle();
+    $("#result1").toggle();
+  });
+  $("#operators").click(function() {
+    $("#term2").toggle();
+    $("#result2").toggle();
+  });
+  $("#variables").click(function() {
+    $("#term3").toggle();
+    $("#result3").toggle();
+  });
+  $("#vNC").click(function() {
+    $("#term4").toggle();
+    $("#result4").toggle();
+  });
+});
